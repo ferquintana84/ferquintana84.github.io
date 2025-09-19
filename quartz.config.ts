956897fec527373b7aa4bb43aa500e73dc8dc6f2
Fernando Quintana <3091621+ferquintana84@users.noreply.github.com>
2025-09-19 17:11:18 +0200

    Quartz sync: Sep 19, 2025, 5:11 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Bitácora del Camino",
+    pageTitle: "Wolf's Garden",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
